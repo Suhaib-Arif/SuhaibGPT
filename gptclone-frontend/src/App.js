5928a987831d2e7f37a568ebb1be971445146ac5
Suhaib-Arif <115211151+Suhaib-Arif@users.noreply.github.com>
2024-09-10 18:07:51 +0530

    Add files via upload

--- gptclone-frontend/src/App.js
+++ gptclone-frontend/src/App.js
@@ -1,19 +1,29 @@
 import "./index.css";
-import gptLogo from "./assets/chatgpt.svg";
+// import gptLogo from "./assets/chatgpt.svg";
 import gptImageLogo from "./assets/chatgptLogo.svg";
-import addbtn from "./assets/add-30.png"
-import sendbtn from "./assets/send.svg"
-import userLogo from "./assets/user-icon.png"
+import addbtn from "./assets/add-30.png";
+import sendbtn from "./assets/send.svg";
 import { useState, useEffect } from "react";
-import ReactMarkdown from 'react-markdown'
-import gfm from 'remark-gfm'
-import attachment from "./assets/paper-clip-svgrepo-com.svg"
+import ReactMarkdown from 'react-markdown';
+import gfm from 'remark-gfm';
+import attachment from "./assets/paper-clip-svgrepo-com.svg";
+import SuhaibGPT from "./assets/SuhaibGPT__2_-removebg-preview (1).png";
+import user from "./assets/person.png";
+import documentlogo from "./assets/document.png"
+
+// import { Document } from 'react-pdf/dist/esm/entry.webpack';
+// import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+
 
 function App() {
   const [text, setText] = useState("");
+  // const [numPages, setNumPages] = useState(null);
+  // const [pageNumber, setPageNumber] = useState(1);
+  // const [fileData, setFileData] = useState(null);
+  const [fileKey, setFileKey] = useState(Date.now());
   const [messages, setMessages] = useState([
     {
-      text: "I'm ChatGPT, an AI language model created by OpenAI. I'm here to help answer your questions, provide information, assist with tasks, or just have a conversation. What can I help you with today?",
+      text: "I'm SuhaibGPT, an AI language model created by OpenAI. I'm here to help answer your questions, provide information, assist with tasks, or just have a conversation. What can I help you with today?",
       isBot: true,
     }
   ]);
@@ -50,9 +60,19 @@ function App() {
       ContactAI(headers, input);
     }else{
       sendPdf(headers, input);
-      setFile(null);
     }
 
+    setFile(null);
+    setFileKey(Date.now());
+  }
+
+  const handleFileUpload = (e) => {
+    setFile(e.target.files[0])
+    // if (file){
+    //   const fileURL = URL.createObjectURL(file);
+    //   setFileData(fileURL);
+    // }
+    
   }
 
   const getSessions =() => {
@@ -110,15 +130,15 @@ function App() {
     console.log("Messages: ",messages);
     setSessionID(response.session_id);
 
-
+    setFile(null);
+    setFileKey(Date.now());
   }
-
 
   return (
     <div className="App">
       <div className="sidebar">
         <div className="uppersidebar">
-          <div className="upperSideTop"><img src={gptLogo} className="logo" alt="img" /><span className="brand">ChatGPT</span></div>
+          <div className="upperSideTop"><img src={SuhaibGPT} className="logo" alt="img" /></div>
           <button className="midBtn" onClick={() => {setSessionID(""); setMessages([]); getSessions();}}><img src={addbtn} alt="" className="addBtn"/>New Chat</button>
 
         </div>
@@ -135,25 +155,35 @@ function App() {
       <div className="main"> 
         <div className="chats">
           {messages.map((message, i) =>
-              <div className={message.isBot? "chat bot": "chat"} style={{textAlign: "left"}}>
-                  <img src={message.isBot ? gptImageLogo : userLogo} className="chatimg" alt="gpt"/><p><ReactMarkdown remarkPlugins={[gfm]} children={message.text}></ReactMarkdown></p>
+              <div className={message.isBot? "chat bot": "chat"}>
+                  <img src={message.isBot ? gptImageLogo : user} className="chatimg" alt="gpt"/><p><ReactMarkdown remarkPlugins={[gfm]} children={message.text}></ReactMarkdown></p>
               </div>
           )}
 
         </div>
 
         <div className="chatFooter">
-          <form onSubmit={handleSubmit}>
-          <div className="inp">
+        <form onSubmit={handleSubmit}>
+        {file && (
+          <div className="document">
+            <div className="close-icon" onClick={()=>{setFile(null); setFileKey(Date.now());}} >&#x2716;</div>
+            <div className="docimg">
+              <img src={documentlogo} alt="img"/>
+            </div>
+            <div className="doctext">
+              {file.name}
+            </div>            
+          </div>
+        )}
+        <div className="inp">
           <div className="file-input-container">
               <label htmlFor="file-input">
                 <img src={attachment} alt="Upload icon" className="icon" />
               </label>
-              <input type="file" accept="application/pdf" id="file-input" onChange={(e) => {setFile(e.target.files[0])}} name="attachment" style={{ display: 'none' }} />
+              <input type="file" accept="application/pdf" id="file-input" onChange={handleFileUpload} name="attachment" key={fileKey} style={{ display: 'none' }} />
           </div>
-            <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Message Chatgpt" name="" id=""/><button type="submit" className="send"><img src={sendbtn} alt="sendbtn"></img></button>
+            <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Message SuhaibGPT" name="" id=""/><button type="submit" className="send"><img src={sendbtn} alt="sendbtn"></img></button>
           </div>
-          <p>ChatGPT may produce incorrent results sometimes.</p>
           </form>
 
         </div>
